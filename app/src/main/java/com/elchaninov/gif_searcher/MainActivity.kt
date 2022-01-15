234681147a5909937c1eaf142a532fe1900ec792
Aleksandr <elchaninov79@gmail.com>
2022-01-15 23:42:18 +0200

    ViewModel for MainActivity created. To pass received data to recyclerview.

--- app/src/main/java/com/elchaninov/gif_searcher/MainActivity.kt
+++ app/src/main/java/com/elchaninov/gif_searcher/MainActivity.kt
@@ -2,15 +2,36 @@ package com.elchaninov.gif_searcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.elchaninov.gif_searcher.databinding.MainActivityBinding
+import com.elchaninov.gif_searcher.ui.main.GifAdapter
+import com.elchaninov.gif_searcher.ui.main.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
 
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java).also {
+            App.instance.component.inject(it)
+        }
+    }
+
+    private var gifAdapter: GifAdapter = GifAdapter(R.layout.item_line_gif)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel.gifs.observe(this, { gifs ->
+            gifAdapter.data = gifs
+        })
+        viewModel.searchGifs()
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = gifAdapter
     }
 }
--- app/src/main/java/com/elchaninov/gif_searcher/ui/main/MainViewModel.kt
+++ app/src/main/java/com/elchaninov/gif_searcher/ui/main/MainViewModel.kt
@@ -1,7 +1,33 @@
 package com.elchaninov.gif_searcher.ui.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.elchaninov.gif_searcher.data.Gif
+import com.elchaninov.gif_searcher.data.GiphyGifRepository
+import io.reactivex.rxjava3.schedulers.Schedulers
+import javax.inject.Inject
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    @Inject
+    lateinit var giphyGifRepository: GiphyGifRepository
+
+    private var _gifs: MutableLiveData<List<Gif>> = MutableLiveData()
+    val gifs: LiveData<List<Gif>> get() = _gifs
+
+    fun searchGifs() {
+        giphyGifRepository.getGifs()
+            .map {
+                Log.d("qqq", "getGifs: ${it}")
+                it
+            }
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                _gifs.postValue(it)
+            }, {
+
+            })
+    }
 }
