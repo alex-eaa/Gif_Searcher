74a4fac887db24bc0dd0b60d03a58e224368503a
Aleksandr <elchaninov79@gmail.com>
2022-01-15 10:21:06 +0200

    main_activity.xml created

--- app/src/main/java/com/elchaninov/gif_searcher/MainActivity.kt
+++ app/src/main/java/com/elchaninov/gif_searcher/MainActivity.kt
@@ -2,17 +2,15 @@ package com.elchaninov.gif_searcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.elchaninov.gif_searcher.ui.main.MainFragment
+import com.elchaninov.gif_searcher.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
