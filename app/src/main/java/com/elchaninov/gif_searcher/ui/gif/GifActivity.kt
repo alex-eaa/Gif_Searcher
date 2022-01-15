8331d16d39502d4294533146ce6d1792de7db5d4
Aleksandr <elchaninov79@gmail.com>
2022-01-16 00:06:38 +0200

    Loading and displaying a Gif in a Gif Activity

--- app/src/main/java/com/elchaninov/gif_searcher/ui/gif/GifActivity.kt
+++ app/src/main/java/com/elchaninov/gif_searcher/ui/gif/GifActivity.kt
@@ -2,6 +2,10 @@ package com.elchaninov.gif_searcher.ui.gif
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.elchaninov.gif_searcher.R
+import com.elchaninov.gif_searcher.data.Gif
 import com.elchaninov.gif_searcher.databinding.GifActivityBinding
 
 
@@ -14,6 +18,17 @@ class GifActivity : AppCompatActivity() {
         binding = GifActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-    }
+        val gif: Gif? = intent.getParcelableExtra("EXTRA_GIF")
 
+        gif?.let{
+            Glide
+                .with(this)
+                .asGif()
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
+                .load(it.urlView)
+                .diskCacheStrategy(DiskCacheStrategy.DATA)
+                .into(binding.gifView)
+        }
+    }
 }
