3a3d1492b61b29aa733de5181c801e2b663bd4a9
Aleksandr <elchaninov79@gmail.com>
2022-01-16 15:07:06 +0200

    Implementation of processing the menu item "layout change"

--- app/src/main/java/com/elchaninov/gif_searcher/MainActivity.kt
+++ app/src/main/java/com/elchaninov/gif_searcher/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -33,8 +35,8 @@ class MainActivity : AppCompatActivity(), GifAdapter.OnItemClickListener {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        gifAdapter = GifAdapter(getItemLayoutForInflate(isLinearLayoutManager), this)
+        initToolbar()
+        initRecyclerView()
 
         viewModel.gifs.observe(this, { gifs ->
             gifAdapter.data = gifs
@@ -43,10 +45,43 @@ class MainActivity : AppCompatActivity(), GifAdapter.OnItemClickListener {
         if (savedInstanceState == null) {
             viewModel.searchGifs()
         }
+    }
 
+    private fun initRecyclerView() {
+        gifAdapter = GifAdapter(getItemLayoutForInflate(isLinearLayoutManager), this)
         binding.recyclerView.layoutManager = getLayoutManager(isLinearLayoutManager)
         binding.recyclerView.adapter = gifAdapter
+
+        viewModel.gifs.value?.let { gifAdapter.data = it }
     }
+
+    private fun initToolbar() {
+        setSupportActionBar(binding.topAppBar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar, menu)
+
+        menu?.let {
+            it.findItem(R.id.action_change_layout)?.setIcon(getIconForChangeLayoutItemMenu())
+        }
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.action_change_layout) {
+            isLinearLayoutManager = !isLinearLayoutManager
+            item.setIcon(getIconForChangeLayoutItemMenu())
+            initRecyclerView()
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+    private fun getIconForChangeLayoutItemMenu(): Int =
+        if (isLinearLayoutManager) R.drawable.ic_baseline_view_agenda_24
+        else R.drawable.ic_baseline_grid_view_24
 
     private fun getItemLayoutForInflate(isLinearLayoutManager: Boolean): Int =
         when (isLinearLayoutManager) {
