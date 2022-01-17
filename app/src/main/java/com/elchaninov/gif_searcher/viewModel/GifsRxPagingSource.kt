4dc6d088e74988e1e6f2a1cd0f8068aad933f43f
Aleksandr <elchaninov79@gmail.com>
2022-01-17 23:15:09 +0200

    Changed settings and data types in PagingSource

--- app/src/main/java/com/elchaninov/gif_searcher/viewModel/GifsRxPagingSource.kt
+++ app/src/main/java/com/elchaninov/gif_searcher/viewModel/GifsRxPagingSource.kt
@@ -2,6 +2,7 @@ package com.elchaninov.gif_searcher.viewModel
 
 import androidx.paging.PagingState
 import androidx.paging.rxjava3.RxPagingSource
+import com.elchaninov.gif_searcher.data.Gif
 import com.elchaninov.gif_searcher.data.mappers.MapGifDtoToGif
 import com.elchaninov.gif_searcher.data.retrofit.GifDto
 import com.elchaninov.gif_searcher.data.retrofit.GiphyApi
@@ -11,37 +12,28 @@ import javax.inject.Inject
 
 class GifsRxPagingSource @Inject constructor(
     val giphyApi: GiphyApi,
-    val query: String,
-    val mapGifDtoToGif: MapGifDtoToGif
-) : RxPagingSource<Int, GifDto.DataDto>() {
+    private val mapGifDtoToGif: MapGifDtoToGif
+) : RxPagingSource<Int, Gif>() {
 
-    val loadSize = 20
-
-    override fun getRefreshKey(state: PagingState<Int, GifDto.DataDto>): Int? {
+    override fun getRefreshKey(state: PagingState<Int, Gif>): Int? {
         return null
     }
 
-    override fun loadSingle(params: LoadParams<Int>): Single<LoadResult<Int, GifDto.DataDto>> {
-        var nextPageNumber: Int? = params.key
-        if (nextPageNumber == null) {
-            nextPageNumber = 0
-        }
+    override fun loadSingle(params: LoadParams<Int>): Single<LoadResult<Int, Gif>> {
+        val position = params.key ?: 0
 
-        return giphyApi.fetchGifsTrending(offset = 0)
-            .map {
-                it
-            }
+        return giphyApi.fetchGifsTrending(offset = position)
             .subscribeOn(Schedulers.io())
             .map { toLoadResult(it) }
             .onErrorReturn { LoadResult.Error(it) }
     }
 
-    private fun toLoadResult(gifDto: GifDto): LoadResult<Int, GifDto.DataDto> {
+    private fun toLoadResult(gifDto: GifDto): LoadResult<Int, Gif> {
         val totalCount = gifDto.pagination.totalCount
         val count = gifDto.pagination.count
         val offset = gifDto.pagination.offset
         return LoadResult.Page(
-            data = gifDto.data,
+            data = mapGifDtoToGif.map(gifDto),
             prevKey = if (offset == 0) null else offset - count,
             nextKey = if (offset == totalCount) null else offset + count
         )
