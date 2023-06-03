6d5e29882ae9cf2aa58e0490678690fe45d02d30
Aleksandr Elchaninov <elchaninov79@gmail.com>
2023-06-03 09:01:50 +0300

    added Subcategory model

--- app/src/main/java/com/elchaninov/gif_searcher/data/api/CategoryResponseDto.kt
+++ app/src/main/java/com/elchaninov/gif_searcher/data/api/CategoryResponseDto.kt
@@ -1,5 +1,7 @@
 package com.elchaninov.gif_searcher.data.api
 
+import com.google.gson.annotations.SerializedName
+
 data class CategoryResponseDto(
     val data: List<CategoryDto>,
 )
@@ -7,5 +9,10 @@ data class CategoryResponseDto(
 data class CategoryDto(
     val name: String,
     val gif: GifDto,
+    val subcategories: List<SubcategoryDto>,
 )
 
+data class SubcategoryDto(
+    val name: String,
+    @SerializedName("name_encoded") val nameEncoded: String,
+)
--- app/src/main/java/com/elchaninov/gif_searcher/model/Category.kt
+++ app/src/main/java/com/elchaninov/gif_searcher/model/Category.kt
@@ -1,14 +1,15 @@
 package com.elchaninov.gif_searcher.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class Category(
     val name: String?,
     val gif: Gif?,
-) : Parcelable {
+    val subcategories: List<Subcategory>,
+) {
     companion object {
-        fun empty() = Category(null, null)
+        fun empty() = Category(null, null, emptyList())
     }
 }
+
+data class Subcategory(
+    val nameEncoded: String,
+)
